c6852564f04d6f79189bd20583fbb0a00581057a
orange95 <b02611006@ntu.edu.tw>
2015-08-16 11:39:12 +0800

    ui done, 存檔的顯示OK了

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextCodec>
+#include "QTime"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    codec = QTextCodec::codecForName("Big5");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString whenSaved="Saved at ";
+    QTime time;
+    time = time.currentTime();
+    whenSaved.append(time.toString("hh:mm"));
+    //qDebug() << time.toString("hh:mm");
+    ui->show->append(whenSaved);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTextCodec>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+     QTextCodec *codec;
 };
 
 #endif // MAINWINDOW_H
